3f12b7284f113f05f3cc48fcc5cc3450f04cbb02
JanisV <ignatov.vit@gmail.com>
2025-12-02 10:27:17 +0300

    Improve processing GitHub markdown format (<p/>)

--- app/repo_engine.py
+++ app/repo_engine.py
@@ -10,7 +10,7 @@ from app.models import Release, Repo
 
 github_extra_html_tags_pattern = re.compile("<p align=\".*?\".*?>|</p>|<a name=\".*?\">|</a>|<picture>.*?</picture>|"
                                             "</?h[1-4]>|</?sub>|</?sup>|</?details>|</?summary>|</?b>|</?dl>|</?dt>|"
-                                            "</?dd>|</?em>|</?small>|<br>|<!--.*?-->",
+                                            "</?dd>|</?em>|</?small>|<br>|<!--.*?-->|<p/>",
                                             flags=re.DOTALL)
 github_img_html_tag_pattern = re.compile("<img .*?src=\"(.*?)\".*?>")
 
