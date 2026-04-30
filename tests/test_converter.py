204e16c9dfd3dbde61ad1c7a8cd7f0d7034bf6e3
JanisV <ignatov.vit@gmail.com>
2026-04-30 22:09:58 +0300

    Add tests

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -106,6 +106,24 @@ def test_format_pre_input(empty_repo, empty_release, case_name):
     assert message == dst_content
 
 @pytest.mark.parametrize("case_name", get_test_cases())
+def test_format_html_input(empty_repo, empty_release, case_name):
+    release_note_format = FORMATTING_PARAMS["html"]["format"]
+
+    orig_path = os.path.join(DATA_DIR, f"{case_name}.orig")
+    dst_path = os.path.join(DATA_DIR, f"{case_name}.html")
+    with open(orig_path, 'r', encoding='utf-8', newline="") as f:
+        orig_content = f.read()
+    with open(dst_path, 'r', encoding='utf-8', newline="") as f:
+        dst_content = f.read()
+
+    empty_release.body = orig_content
+
+    message, parse_mode, entities = format_release_message(release_note_format, empty_repo, empty_release)
+
+    assert parse_mode in (FORMATTING_PARAMS["html"]["mode"], FORMATTING_PARAMS["markdown"]["mode"])
+    assert message == dst_content
+
+@pytest.mark.parametrize("case_name", get_test_cases())
 def test_format_markdown_input(empty_repo, empty_release, case_name):
     release_note_format = FORMATTING_PARAMS["markdown"]["format"]
 
